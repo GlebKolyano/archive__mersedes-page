82d29dc4b1775f33ea1f53cd60ce4266f0ecf7b1
GlebKolyano <glebkolyano38@gmail.com>
2021-09-27 17:33:41 +0300

    made the burget menu js / finished

--- js/burgerMenu.js
+++ js/burgerMenu.js
@@ -1,1 +1,24 @@
 const burgerBtn = document.querySelector('.header__humburger')
+const menu = document.querySelector('.header__menu')
+
+
+
+burgerBtn.addEventListener('click', () => {
+  menu.classList.toggle('header__menu-active')
+  burgerBtn.classList.toggle('header__humburger-active')
+
+  burgerBtn.classList.toggle('header__humburger')
+})
+
+
+menu.addEventListener('click', event => {
+  
+
+  if (event.target != menu) {
+    menu.classList.toggle('header__menu-active')
+    burgerBtn.classList.toggle('header__humburger-active')
+
+    burgerBtn.classList.toggle('header__humburger')
+  }
+})
+
