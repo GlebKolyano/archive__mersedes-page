502693e267ebb41975753c91fc71f6dedec4d2f0
GlebKolyano <glebkolyano38@gmail.com>
2021-09-27 15:15:23 +0300

    ?

--- js/tabs.js
+++ js/tabs.js
@@ -1,17 +1,18 @@
 const getBtns = document.querySelectorAll('[data-tabs-handler]')
 const getElem = document.querySelectorAll('[data-tabs-field]')
 
-getBtns.forEach((button) => {
+
+for (const button of getBtns) {
   button.addEventListener('click', () => {
     getBtns.forEach(item => item.classList.remove('design-list__item_active'))
     button.classList.add('design-list__item_active')
-    
-    getElem.forEach((element) => {
-      if (element.dataset.tabsField === button.dataset.tabsHandler) {
+
+    getElem.forEach(element => {
+      if(element.dataset.tabsField === button.dataset.tabsHandler) {
         element.classList.remove('hide')
       } else {
         element.classList.add('hide')
       }
     })
   })
-})
+}
