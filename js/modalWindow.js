abf29bc713281781a999ed991381916396f3c843
GlebKolyano <glebkolyano38@gmail.com>
2021-09-27 16:03:54 +0300

    made a call the modal window JS / finished

--- js/modalWindow.js
+++ js/modalWindow.js
@@ -1,1 +1,15 @@
 const modalBtn = document.querySelector('.design__more')
+const modalWin = document.querySelector('.modal')
+
+
+modalBtn.addEventListener('click', () => {
+  modalWin.classList.remove('hide')
+})
+
+modalWin.addEventListener('click', event => {
+  const target = event.target
+  
+  if (target.classList.contains('modal__close') || target.classList.contains('overlay')) {
+    modalWin.classList.add('hide')
+  }
+})
